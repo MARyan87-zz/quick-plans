a7d8cfc319f06337afefba2c65c662af58a7cb0d
Michael Ryan <mickeyryan1987@gmail.com>
2016-09-01 21:04:06 -0400

    updates to plans.js

--- assets/js/plans.js
+++ assets/js/plans.js
@@ -1,3 +1,14 @@
-$.get(api, function(){
+$('#submit-button').on('click', function(){
+	var startDate = $('#date-input').val();
+	var zipCode = $('#zip-input').val();
 
+	var api = 'data.tmsapi.com/v1.1/movies/showings?startDate=' + startDate + '&zip=' + zipCode + '&api_key=3c7u9b4fnquyfbbkqzc2tzgj';
+
+
+	$.ajax({url: api, method: 'GET'})
+		.done(function(data){
+			
+
+	});
 });
+
