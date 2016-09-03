59c7617ef54a409da2238caf164ee1f7f2ffab91
Michael Ryan <mickeyryan1987@gmail.com>
2016-09-03 02:36:02 -0400

    added styling and plot for movies

--- assets/js/plans.js
+++ assets/js/plans.js
@@ -40,23 +40,33 @@ var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query
 			.done(function(data){
 				console.log(data);
 				
-				var movieList = $('<ul id="movieList">');
+				var movieList = $('<ul class="panel-body list-group" id="movieList">');
 				for (var i=0; i<data.length; i++){
 					var rating = "Not Rated";
 					if (data[i].hasOwnProperty("ratings")){
 						var rating = data[i].ratings[0].code;
-						console.log(rating);
 					}
 
+					$(movieList).append('<li class="movieItem list-group-item"><h3>' + data[i].title +
+										 '<span class="expand">+</span></h3>'+'<p class=rating>' + rating + 
+										 '</p>' + '<section class="hide" id="movie' + i + '">');
 
-					
-					
-					$(movieList).append('<li class="movieItem"><h3>' + data[i].title + '</h3>' + 
-											 '<section id="movie">' + '<p class=rating>' + rating);
 
-					
 				}
-				$('#movieDisplay').append(movieList);
+				$('#movieDisplay').html(movieList);
+				for (var i=0; i<data.length; i++){
+					var currentMovie = '#movie' + i,
+						plot = data[i].shortDescription,
+						website = data[i].officialUrl;
+
+
+					$(currentMovie).html('<p id="plot">' + plot);
+					$(currentMovie).append('<a target="_blank" href="' + website + '">Official Website</a>');
+				}
 		});
 	});
 });//end doc.ready
+
+$(document).on('click', '.movieItem', function(){
+	$(this).find('section').toggleClass('hide');
+})
